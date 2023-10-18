499dc2cc50d3519c1f7fbfcf1ef6ed9acdf21203
Bray Moll <bkm82@nau.edu>
2023-10-18 00:41:21 -0700

    create github actions

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -9,20 +9,20 @@ from solver.foo import add_one, main
     (-1, 0),
 ])
 @pytest.hookimpl(tryfirst=True)
-def test_add_one(test_input,expected):
+def test_add_one(test_input, expected):
     assert add_one(test_input) == expected
 
 
 @pytest.fixture
 def capture_stdout(monkeypatch):
-    buffer = {"stdout": "", "write_calls":0}
+    buffer = {"stdout": "", "write_calls": 0}
 
     def fake_write(s):
         buffer["stdout"] += s
         buffer["write_calls"] += 1
 
     monkeypatch.setattr(sys.stdout, 'write', fake_write)
-    return(buffer)
+    return (buffer)
 
 
 def test_print(capture_stdout):
