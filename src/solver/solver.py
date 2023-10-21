82ddf43ca11c840ba7ccbe5d9e07d51b1e119470
Bray Moll <bkm82@nau.edu>
2023-10-21 14:24:50 -0700

    refactor implicit solver, add naming clarification TODO

--- src/solver/solver.py
+++ src/solver/solver.py
@@ -16,19 +16,19 @@ class solver_1d:
         k = self.mesh.thermal_diffusivity * delta_t / (self.mesh.delta_x**2)
         identity_matrix = np.identity(self.mesh.n_cells)
         current_temperature = self.mesh.temperature
+        differentiation_matrix = k * self.mesh.differentiation_matrix
+
         if self.method == "explicit":
             # solve the form y = ax + b
-            a = (k * self.mesh.differentiation_matrix) + identity_matrix
+            a = differentiation_matrix + identity_matrix
             b = k * self.mesh.boundary_condition_array
             self.mesh.temperature = a @ current_temperature + b
 
         if self.method == "implicit":
-            # solve the form ay = bx where x = current temp, y= new temp
-
-            a = identity_matrix - (k * self.mesh.differentiation_matrix)
-            b = self.mesh.temperature + (k * self.mesh.boundary_condition_array)
-
-            self.mesh.temperature = np.linalg.solve(a, b)
+            # solve the form ay = x+b where x= current temp, y= new temp
+            a = -differentiation_matrix + identity_matrix
+            b = k * self.mesh.boundary_condition_array
+            self.mesh.temperature = np.linalg.solve(a, (current_temperature + b))
 
 
 # def main():
