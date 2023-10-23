9b678f80760fd6ae48a8d7ed0ca4ef82a7d234b6
Bray Moll <bkm82@nau.edu>
2023-10-22 18:15:02 -0700

    incorporate mesh_type to main scrip, removed duplicate tests

--- src/solver/hw_5_dirichlet_neumann.py
+++ src/solver/hw_5_dirichlet_neumann.py
@@ -8,8 +8,8 @@ import pandas as pd
 logging.basicConfig(level=logging.WARNING)
 
 
-def create_mesh(n_cells):
-    mesh = mesher.create_1Dmesh(x=[0, 1], n_cells=n_cells)
+def create_mesh(n_cells, mesh_type):
+    mesh = mesher.create_1Dmesh(x=[0, 1], n_cells=n_cells, mesh_type=mesh_type)
     mesh.set_cell_temperature(0)  # set initial conditions to 0 celcicus
     mesh.set_dirichlet_boundary("left", 50)  # Left to 50 c
     mesh.set_neumann_boundary("right")
@@ -19,13 +19,14 @@ def create_mesh(n_cells):
 
 def main():
     n_cells = 20
-    time_step_size = 15  # seconds
-    time_max = 30000  # seconds
-
-    print(create_mesh(4).delta_x)
+    # time_step_size = 15  # seconds
+    time_max = 30  # seconds $30000
+    time_max_explore = 60000
+    mesh_type = "finite_difference"
+    print(create_mesh(4, mesh_type).delta_x)
 
     explicit_solution_unstable = solver.solver_1d(
-        mesh=create_mesh(n_cells),
+        mesh=create_mesh(n_cells, mesh_type),
         initial_time=0,
         time_step_size=15,
         method="explicit",
@@ -33,7 +34,7 @@ def main():
     explicit_solution_unstable.solve(60)
 
     explicit_solution_stable = solver.solver_1d(
-        mesh=create_mesh(n_cells),
+        mesh=create_mesh(n_cells, mesh_type),
         initial_time=0,
         time_step_size=1,
         method="explicit",
@@ -41,21 +42,21 @@ def main():
     explicit_solution_stable.solve(time_max)
 
     implicit_solution_15sec = solver.solver_1d(
-        mesh=create_mesh(n_cells),
+        mesh=create_mesh(n_cells, mesh_type),
         initial_time=0,
         time_step_size=15,
         method="implicit",
     )
-    implicit_solution_15sec.solve(time_max)
+    implicit_solution_15sec.solve(time_max_explore)
 
     implicit_solution_1sec = solver.solver_1d(
-        mesh=create_mesh(n_cells),
+        mesh=create_mesh(n_cells, mesh_type),
         initial_time=0,
         time_step_size=1,
         method="implicit",
     )
     implicit_solution_1sec.solve(time_max)
-    logging.debug(
+    logging.warning(
         f"explicit_solution saved data \n {explicit_solution_stable.saved_data.head(10)}"
     )
 
@@ -79,6 +80,7 @@ def main():
         600,
         3600,
         time_max,
+        time_max_explore,
     ]  # time points that you want to plot
 
     plot_data_filterd_bool = plot_data["time"].isin(time_points)
